2758ae7facefe6cbcd5d307899b75547c2237b01
Cai Jenkins <Caisjenkins@gmail.com>
2020-08-20 13:07:16 +0100

    simple button using redux hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import './App.css';
 
 function App() {
+  const counter = useSelector(state => state);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>{counter.num}</h1>
+      <button 
+        onClick={() => 
+          dispatch({
+            type: "INCREMENT",
+            step: 1
+          })
+        }
         >
-          Learn React
-        </a>
-      </header>
+          Increment
+        </button>
     </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import './index.css';
+import reducer from "./store.js";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
